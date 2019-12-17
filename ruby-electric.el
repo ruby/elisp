c1ab53a563670ef66e07607a1ca7a1075d169f28
Akinori MUSHA <knu@idaemons.org>
2019-12-17 21:11:44 +0900

    Mention enh-ruby-mode

--- ruby-electric.el
+++ ruby-electric.el
@@ -27,6 +27,11 @@
 ;;     (eval-after-load "ruby-mode"
 ;;       '(add-hook 'ruby-mode-hook 'ruby-electric-mode))
 ;;
+;; Or add the following line for enh-ruby-mode.
+;;
+;;     (eval-after-load "enh-ruby-mode"
+;;       '(add-hook 'enh-ruby-mode-hook 'ruby-electric-mode))
+;;
 ;; Type M-x customize-group ruby-electric for configuration.
 
 ;;; Code:
