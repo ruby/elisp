37e4559aedc08c281ec5da30482712df3736099c
Nobuyoshi Nakada <nobu@ruby-lang.org>
2023-01-30 14:29:13 +0900

    Support output from `test_` prefixed methods

--- ruby-mkmf-log-mode.el
+++ ruby-mkmf-log-mode.el
@@ -22,10 +22,10 @@ addition to have_/check_/find_ methods")
   (concat
    "^\\(?:"
    "\\(?:"
-   "\\(?:have_\\|check_\\|find_\\)[_a-zA-Z0-9]*\\??\\|"
+   "\\(?:test_\\|have_\\|check_\\|find_\\)[_a-zA-Z0-9]*\\??\\|"
    (regexp-opt ruby-mkmf-log-methods)
    "\\)"
-   ": \\)?checking .*\\.\\.\\. \\(--------------------\\) \\(.*\\)")
+   ": \\)?checking .*?\\(?: in \\(.*\\)\\)?\\.\\.\\. \\(--------------------\\) \\(.*\\)")
   "Regexp to match the beginning of each checks")
 
 (defconst ruby-mkmf-log-end-re
@@ -50,14 +50,16 @@ addition to have_/check_/find_ methods")
   "Toggle visibility of the current check"
   (interactive)
   (save-excursion
-    (let ((ovs (overlays-at (point-at-eol))))
+    (let ((ovs (overlays-at (point))))
       (mapcar
        (lambda (o)
-	 (if (overlay-get o 'hide-ruby-mkmf-log)
+	 (setq o (overlay-get o 'hide-ruby-mkmf-log))
+	 (if (overlayp o)
 	     (let ((visible (overlay-get o 'invisible)))
 	       (overlay-put o 'invisible (not visible))
-	       (overlay-put o 'before-string
-			    (if visible ruby-mkmf-log-visible-mark ruby-mkmf-log-hidden-mark)))))
+	       ;; (overlay-put o 'before-string
+	       ;; 		    (if visible ruby-mkmf-log-visible-mark ruby-mkmf-log-hidden-mark))
+	       )))
        ovs))))
 
 (defvar ruby-mkmf-log-overlay-keymap (make-keymap) "Keymap to toggle checks")
@@ -76,10 +78,10 @@ addition to have_/check_/find_ methods")
 	 (delete-overlay o)))
    (overlays-in start end)))
 
-(defun ruby-mkmf-log-make-overlay (start end &optional front-advance rear-advance no-keymap)
+(defun ruby-mkmf-log-make-overlay (ov start end &optional front-advance rear-advance no-keymap)
   (let ((o (make-overlay start end nil front-advance rear-advance)))
     (overlay-put o 'category 'ruby-mkmf-log)
-    (overlay-put o 'hide-ruby-mkmf-log t)
+    (overlay-put o 'hide-ruby-mkmf-log (or ov o))
     (unless no-keymap
       (overlay-put o 'keymap ruby-mkmf-log-overlay-keymap))
     o))
@@ -94,36 +96,44 @@ addition to have_/check_/find_ methods")
     (or end (setq end (point-max)))
     (goto-char (or start (point-min)))
     (ruby-mkmf-log-hide-clear (point) end)
-    (let (s b e b1 e1 b2 e2 o result)
+    (let (s b e b1 e1 b2 e2 bh eh o result ov)
       (while (and (setq b (re-search-forward ruby-mkmf-log-methods-re end t)
-			b1 (match-beginning 1) e1 (match-end 1)
-			b2 (match-beginning 2) e2 (match-end 2)
-			result (match-string 2)
+			bh (match-beginning 1) eh (match-end 1)
+			b1 (match-beginning 2) e1 (match-end 2)
+			b2 (match-beginning 3) e2 (match-end 3)
+			result (match-string 3)
 			s (point-at-bol))
 		  (setq e (re-search-forward ruby-mkmf-log-end-re end t)))
-	(setq o (ruby-mkmf-log-make-overlay s b))
+	(setq ov (ruby-mkmf-log-make-overlay nil b e t))
+	(overlay-put ov 'face ruby-mkmf-log-check-content-face)
+	;; (overlay-put ov 'before-string ruby-mkmf-log-hidden-mark)
+	(overlay-put ov 'invisible 'hide-ruby-mkmf-log)
+
+	(setq o (ruby-mkmf-log-make-overlay ov s b))
 	(overlay-put o 'face ruby-mkmf-log-check-heading-face)
-	(setq o (ruby-mkmf-log-make-overlay b1 e1 nil nil t))
+	(when bh
+	    (setq o (ruby-mkmf-log-make-overlay t bh eh))
+	    (overlay-put o 'before-string ruby-mkmf-log-hidden-mark)
+	    (overlay-put o 'help-echo (match-string 1))
+	    (overlay-put o 'invisible 'hide-ruby-mkmf-log)
+	    )
+	(setq o (ruby-mkmf-log-make-overlay ov b1 e1 nil nil t))
 	(overlay-put o 'face ruby-mkmf-log-check-content-face)
-	(setq o (ruby-mkmf-log-make-overlay b2 e2 nil nil t))
+	(setq o (ruby-mkmf-log-make-overlay ov b2 e2 nil nil t))
 	(let ((face
 	       (cond
 		((string= result "yes") 'compilation-info)
 		((string= result "no") 'compilation-error)
 		(t 'compilation-warning))))
 	 (overlay-put o 'face face))
-	(setq o (ruby-mkmf-log-make-overlay b e t))
-	(overlay-put o 'face ruby-mkmf-log-check-content-face)
-	(overlay-put o 'before-string ruby-mkmf-log-hidden-mark)
-	(overlay-put o 'invisible 'hide-ruby-mkmf-log)
 	))
     (when (re-search-forward "^\\(extconf\\.h\\) is:\n/\\* begin \\*/\n\\(\\(?:.*\n\\)*\\)/\\* end \\*/$" end t)
-      (let ((o (ruby-mkmf-log-make-overlay (match-beginning 1) (match-end 1))))
+      (let ((o (ruby-mkmf-log-make-overlay t (match-beginning 1) (match-end 1))))
 	(overlay-put o 'face 'compilation-info))
       (let ((b (match-beginning 2)) (e (match-end 2)))
 	(goto-char b)
 	(while (re-search-forward "^ *[1-9][0-9]*: " e t)
-	  (let ((o (ruby-mkmf-log-make-overlay (match-beginning 0) (match-end 0))))
+	  (let ((o (ruby-mkmf-log-make-overlay t (match-beginning 0) (match-end 0))))
 	    (overlay-put o 'face 'compilation-line-number)))))
     ))
 
